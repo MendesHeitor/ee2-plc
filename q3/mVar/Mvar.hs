61a91379c6298ea405b897abaa6bf3ad89962709
Eduardo Amorim <72039442+Edu-p@users.noreply.github.com>
2023-09-26 07:16:14 -0300

    fix: q4

--- q3/mVar/Mvar.hs
+++ q3/mVar/Mvar.hs
@@ -1,32 +1,23 @@
--- Importando módulos necessários para concorrência e funções utilitárias
 import Control.Concurrent
 import Control.Monad
 import Data.Maybe
 
--- Definindo um tipo `Vaga` que pode ser PCD ou Comum
 data Vaga = PCD | Comum deriving (Show, Eq)
 
--- Definindo um tipo `Carro` que pode ser um CarroPCD com um identificador ou um CarroComum com um identificador
 data Carro = CarroPCD Int | CarroComum Int deriving (Show, Eq)
 
--- Função principal do programa
 main :: IO ()
 main = do
-    -- Solicita ao usuário o número de carros e lê o input
     putStrLn "Digite o número N de carros:"
     n <- readLn
     
-    -- Solicita ao usuário o número de vagas e lê o input
     putStrLn "Digite o número K de vagas:"
     k <- readLn
     
-    -- Cria um MVar (variável mutável) contendo a lista de vagas, sendo 10% PCD e o resto Comum
     vagas <- newMVar (replicate (k `div` 10) PCD ++ replicate (9 * k `div` 10) Comum)
     
     threadCounter <- newMVar n 
     
-    -- Para cada carro, se for PCD (20% primeiros carros), inicia uma thread para monitorar esse carro em uma vaga PCD.
-    -- Senão, inicia uma thread para monitorar esse carro em uma vaga comum.
     forM_ [1..n] $ \i -> do
         _ <- forkIO $ do
             if i <= n `div` 5 then
@@ -38,13 +29,12 @@ main = do
 
     waitForThreads threadCounter
 
--- Espera até que todas as threads tenham concluído
 waitForThreads :: MVar Int -> IO ()
 waitForThreads threadCounter = do
     remaining <- takeMVar threadCounter
     if remaining > 0 then do
         putMVar threadCounter remaining
-        threadDelay 1000 -- espera 0,1 segundo antes de verificar novamente
+        threadDelay 1000 
         waitForThreads threadCounter
     else
         return ()
@@ -92,7 +82,6 @@ monitorarCarro carro vagasMVar = do
                     threadDelay 5000
                     monitorarCarro carro vagasMVar
                     
--- Função auxiliar para pegar uma vaga da lista e retornar essa vaga junto com a lista restante
 pegarVaga :: Vaga -> [Vaga] -> (Maybe Vaga, [Vaga])
 pegarVaga _ [] = (Nothing, [])
 pegarVaga vaga (x:xs)
