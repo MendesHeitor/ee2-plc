7342964b3dacb4e667c70ab0f080a6527a290b3e
Heitor Mendes Pereira <hemepe18@gmail.com>
2023-09-26 07:46:13 -0300

    last changes

--- q3/mVar/MVAR.hs
+++ q3/mVar/MVAR.hs
@@ -1,96 +1,80 @@
 import Control.Concurrent
 import Control.Monad
 import Data.Maybe
+import Control.Concurrent.MVar
+import Control.Concurrent (threadDelay)
 
 data Vaga = PCD | Comum deriving (Show, Eq)
-
 data Carro = CarroPCD Int | CarroComum Int deriving (Show, Eq)
 
-main :: IO ()
-main = do
-    putStrLn "Digite o número N de carros:"
-    n <- readLn
-    
-    putStrLn "Digite o número K de vagas:"
-    k <- readLn
-    
-    vagas <- newMVar (replicate (k `div` 10) PCD ++ replicate (9 * k `div` 10) Comum)
-    
-    threadCounter <- newMVar n 
-    
-    forM_ [1..n] $ \i -> do
-        _ <- forkIO $ do
-            if i <= n `div` 5 then
-                monitorarCarro (CarroPCD i) vagas
-            else
-                monitorarCarro (CarroComum i) vagas
-            modifyMVar_ threadCounter (return . subtract 1)
-        return ()
-
-    waitForThreads threadCounter
-
-waitForThreads :: MVar Int -> IO ()
-waitForThreads threadCounter = do
-    remaining <- takeMVar threadCounter
-    if remaining > 0 then do
-        putMVar threadCounter remaining
-        threadDelay 1000 
-        waitForThreads threadCounter
-    else
-        return ()
-
-
-
-monitorarCarro :: Carro -> MVar [Vaga] -> IO ()
-monitorarCarro carro vagasMVar = do
-    vagas <- takeMVar vagasMVar
-    case carro of
-        CarroPCD i -> do
-            let (vagaOcupada, restante) = pegarVaga PCD vagas
-            case vagaOcupada of
-                Just vaga -> do
-                    putMVar vagasMVar restante
-                    putStrLn $ "Carro PCD " ++ show i ++ " pegou vaga " ++ show vaga
-                    threadDelay 1500
-                    putStrLn $ "Carro PCD " ++ show i ++ " liberou vaga " ++ show vaga
-                    putMVar vagasMVar (vaga:restante)
-                Nothing -> do
-                    let (vagaOcupadaComum, restanteComum) = pegarVaga Comum vagas
-                    case vagaOcupadaComum of
-                        Just vagaComum -> do
-                            putMVar vagasMVar restanteComum
-                            putStrLn $ "Carro PCD " ++ show i ++ " pegou vaga " ++ show vagaComum
-                            threadDelay 1500
-                            putStrLn $ "Carro PCD " ++ show i ++ " liberou vaga " ++ show vagaComum
-                            putMVar vagasMVar (vagaComum:restanteComum)
-                        Nothing -> do
-                            putMVar vagasMVar vagas
-                            threadDelay 5000
-                            monitorarCarro carro vagasMVar
-
-        CarroComum i -> do
-            let (vagaOcupada, restante) = pegarVaga Comum vagas
-            case vagaOcupada of
-                Just vaga -> do
-                    putMVar vagasMVar restante
-                    putStrLn $ "Carro comum " ++ show i ++ " pegou vaga " ++ show vaga
-                    threadDelay 1000
-                    putStrLn $ "Carro comum " ++ show i ++ " liberou vaga " ++ show vaga
-                    putMVar vagasMVar (vaga:restante)
-                Nothing -> do
-                    putMVar vagasMVar vagas
-                    threadDelay 5000
-                    monitorarCarro carro vagasMVar
-                    
 pegarVaga :: Vaga -> [Vaga] -> (Maybe Vaga, [Vaga])
 pegarVaga _ [] = (Nothing, [])
 pegarVaga vaga (x:xs)
-    | vaga == x = (Just x, xs)
+    | (vaga == PCD) || (vaga == x) = (Just x, xs)
     | otherwise = let (v, ys) = pegarVaga vaga xs in (v, x : ys)
 
 
-printVar :: [Vaga] -> IO ()
-printVar [] = return ()
-printVar (x:xs) = do
-    print x
-    printVar xs
+monitorarCarro :: MVar [Vaga] -> Carro -> IO ()
+monitorarCarro vagasMVar carro = do
+    vagas <- takeMVar vagasMVar 
+
+    case carro of
+        CarroPCD i -> do
+            let (mVaga, vagasAtualizadas) = pegarVaga PCD vagas
+            case mVaga of
+                Just obtainedVaga -> do
+                    case obtainedVaga of
+                        PCD -> do
+                            putMVar vagasMVar vagasAtualizadas
+                            putStrLn $ "Carro PCD " ++ show i ++ " pegou vaga " ++ show obtainedVaga
+                            threadDelay 1000
+                            putStrLn $ "Carro PCD " ++ show i ++ " liberou vaga " ++ show obtainedVaga
+                            vagas <- takeMVar vagasMVar
+                            putMVar vagasMVar (obtainedVaga:vagas)
+                        Comum -> do
+                            putMVar vagasMVar vagasAtualizadas
+                            putStrLn $ "Carro PCD " ++ show i ++ " pegou vaga " ++ show obtainedVaga
+                            threadDelay 1500
+                            putStrLn $ "Carro PCD " ++ show i ++ " liberou vaga " ++ show obtainedVaga
+                            vagas <- takeMVar vagasMVar
+                            putMVar vagasMVar (obtainedVaga:vagas)
+                Nothing -> do
+                    putMVar vagasMVar vagas
+                    monitorarCarro vagasMVar carro
+
+        CarroComum i -> do
+            let (mVaga, vagasAtualizadas) = pegarVaga Comum vagas
+            case mVaga of
+                Just obtainedVaga -> do
+                    putMVar vagasMVar vagasAtualizadas
+                    putStrLn $ "Carro comum " ++ show i ++ " pegou vaga " ++ show obtainedVaga
+                    threadDelay 1000
+                    putStrLn $ "Carro comum " ++ show i ++ " liberou vaga " ++ show obtainedVaga
+                    vagas <- takeMVar vagasMVar
+                    putMVar vagasMVar (obtainedVaga:vagas)
+                Nothing -> do
+                    putMVar vagasMVar vagas
+                    monitorarCarro vagasMVar carro
+
+           
+main :: IO ()
+main = do
+
+    putStrLn "Digite o número N de carros:"
+    n <- readLn
+
+    putStrLn "Digite o número K de vagas:"
+    k <- readLn
+
+    let vagasPCD = k `div` 10
+    let vagasComum = k - vagasPCD
+    
+    vagas <- newMVar (replicate vagasPCD PCD ++ replicate vagasComum Comum)
+
+    forM_ [1..n] $ \i ->
+        if i <= n `div` 5 then
+            forkIO (monitorarCarro vagas (CarroPCD i))
+        else
+            forkIO (monitorarCarro vagas (CarroComum i))
+
+    threadDelay (n * 1000000)
--- q4/java/PingPong.java
+++ q4/java/PingPong.java
@@ -1,47 +1,39 @@
-package java;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class PingPong {
-    private static Object lock = new Object();
-    private static boolean messageSent = false;
+    private static final String PING = " Ping!";
+    private static final String PONG = "Pong!";
+    private static final BlockingQueue<String> pingQueue = new LinkedBlockingQueue<>(1); 
+    private static final BlockingQueue<String> pongQueue = new LinkedBlockingQueue<>(1); 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         System.out.println("Digite valor de N:");
         int N = scan.nextInt();
         scan.close();
 
         Thread ping = new Thread(() -> {
-            synchronized (lock) {
-                for (int i = 0; i < N; i++) {
-                    while (messageSent) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    System.out.println("Ping!");
-                    messageSent = true;
-                    lock.notifyAll();
+            for (int i = 0; i < N; i++) {
+                try {
+                    pingQueue.put(PING); // coloca ping
+                    System.out.println(PING);
+                    while (!pongQueue.take().equals(PONG)) { } // trava enquanto pong não é colocado
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
 
         Thread pong = new Thread(() -> {
-            synchronized (lock) {
-                for (int i = 0; i < N; i++) {
-                    while (!messageSent) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    System.out.println(" Pong!");
-                    messageSent = false;
-                    lock.notifyAll();
+            for (int i = 0; i < N; i++) {
+                try {
+                    while (!pingQueue.take().equals(PING)) { } //trava enquanto ping não é colocado
+                    System.out.println(PONG);
+                    pongQueue.put(PONG); // coloca pong
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
